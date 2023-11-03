507cdc5b0109dc868a38da4df8319d7fc4a091e6
WagaoFC <wagnerferreira_07@hotmail.com>
2023-11-03 18:50:48 -0300

    :sparkles: feat: searching for repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react"
+
 export function App() {
+  interface IRepos {
+    name: string,
+  }
+
+  interface ILanguages {
+    tech: string,
+  }
+
+  const [username, setUsername] = useState('')
+  const [languageCollection, setLanguageCollection] = useState<ILanguages[]>([])
+
+  useEffect(() => {
+    console.log(languageCollection)
+  }, [languageCollection])
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    try {
+      const res = await fetch(`https://api.github.com/users/${username}/repos`)
+      const repos = await res.json()
+      const nameRepos = repos.map((m: IRepos) => m.name)
+
+      for (const repo of nameRepos) {
+        const res = await fetch(`https://api.github.com/repos/${username}/${repo}/languages`)
+        const languages = await res.json()
+  
+        setLanguageCollection(previous => [...previous, languages])
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} 
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
+    </>
   )
 }
