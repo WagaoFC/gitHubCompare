f3b9bbd6a48369c4e503e5fd0ec5c092f42bb7f6
WagaoFC <wagnerferreira_07@hotmail.com>
2023-11-07 21:32:39 -0300

    :bug: fix: adjusting initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { Chart } from "./components/Chart";
+import { Mic, Search } from 'lucide-react'
+import { Chart } from './components/Chart'
 
 export function App() {
   return (
-    <div className="bg-background">
-      <header className="bg-current-line p-4">
+    <div className='bg-background h-screen'>
+      <header className='bg-current-line p-4'>
         header
       </header>
-      <main className="p-4">
-        <form className="flex justify-center items-center">
-          <input
-            type="text"
-          />
-          <button type="submit">
-            Search
-          </button>
-        </form>
-        <div className="max-w-4xl">
-          <Chart />
-        </div>
-      </main>
+      <body>
+        <main className='p-4 space-y-6'>
+          <div className='flex gap-2 w-full items-center justify-center'>
+            <div className='flex w-full max-w-xs items-center gap-2 rounded-lg border border-current-line px-3 py-2 shadow-sm'>
+              <Search className='h-5 w-5 text-current-line' />
+              <input
+                className='flex-1 border-0 bg-transparent p-0 text-current-line placeholder-current-line focus:outline-none'
+                placeholder='username'
+              />
+            </div>
+            <button className='bg-transparent text-current-line border border-current-line rounded-lg px-3 py-2 shadow-sm'>
+              <Mic />
+            </button>
+          </div>
+          <div className='max-w-4xl'>
+            <Chart />
+          </div>
+        </main>
+      </body>
     </div>
-  );
+  )
 }
