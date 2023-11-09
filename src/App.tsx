d71067d3eaf25c30fa7159f34449127e2c0c99dc
WagaoFC <wagnerferreira_07@hotmail.com>
2023-11-09 01:27:25 -0300

    :sparkles: feat: add component Users

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Mic, Search } from 'lucide-react'
 import { Chart } from './components/Chart'
+import { Users } from './components/Users'
 
 export function App() {
   return (
@@ -7,25 +8,28 @@ export function App() {
       <header className='bg-current-line p-4'>
         header
       </header>
-      <body>
-        <main className='p-4 space-y-6'>
-          <div className='flex gap-2 w-full items-center justify-center'>
-            <div className='flex w-full max-w-xs items-center gap-2 rounded-lg border border-current-line px-3 py-2 shadow-sm'>
-              <Search className='h-5 w-5 text-current-line' />
-              <input
-                className='flex-1 border-0 bg-transparent p-0 text-current-line placeholder-current-line focus:outline-none'
-                placeholder='username'
-              />
-            </div>
-            <button className='bg-transparent text-current-line border border-current-line rounded-lg px-3 py-2 shadow-sm'>
-              <Mic />
-            </button>
+      <main className='p-4 space-y-6'>
+        <div className='flex gap-2 w-full items-center justify-center'>
+          <div className='flex w-full max-w-xs items-center gap-2 rounded-lg border border-current-line px-3 py-2 shadow-sm'>
+            <Search className='h-5 w-5 text-current-line' />
+            <input
+              className='flex-1 border-0 bg-transparent p-0 text-current-line placeholder-current-line focus:outline-none'
+              placeholder='username'
+            />
           </div>
-          <div className='max-w-4xl'>
+          <button className='bg-transparent text-current-line border border-current-line rounded-lg px-3 py-2 shadow-sm'>
+            <Mic />
+          </button>
+        </div>
+        <div className='min-h-screen grid grid-cols-2'>
+          <div>
             <Chart />
           </div>
-        </main>
-      </body>
+          <div>
+            <Users />
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
---
+++ src/components/Users/index.tsx
@@ -0,0 +1,21 @@
+export function Users() {
+    return (
+        <div className="flex items-center justify-center">
+            <img
+                className="rounded-full w-16"
+                src="https://avatars.githubusercontent.com/u/48607604?v=4"
+            />
+            <section className="flex flex-col">
+                <span>
+                    WagaoFC
+                </span>
+                <span>
+                    Front-end Developer
+                </span>
+            </section>
+            <button>
+                Add
+            </button>
+        </div>
+    )
+}
